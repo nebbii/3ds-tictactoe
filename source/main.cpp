4b6b81c3fb6bb89d1e99f434667435ba0ae80da0
Ben W <ben.wolthuis@gmail.com>
2022-10-02 23:57:42 +0200

    touch screen input handling implemented

--- source/main.cpp
+++ source/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 std::string renderPlayfield(int field[3][3]) {
-    std::string pf = "\n";
+    std::string pf;
 
     for(int y=0; y<3; y++) {
         for(int x=0; x<3; x++) {
@@ -24,6 +24,31 @@ std::string renderPlayfield(int field[3][3]) {
     return pf;
 }
 
+int *handleInput(u16 px, u16 py) {
+    static int slot[] = {0, 0};
+    if (px < 8) {
+        slot[0] = 0;
+    }
+    else if (px < 16) {
+        slot[0] = 1;
+    }
+    else if (px < 24) {
+        slot[0] = 2;
+    }
+
+    if (py < 8) {
+        slot[1] = 0;
+    }
+    else if (py < 16) {
+        slot[1] = 1;
+    }
+    else if (py < 24) {
+        slot[1] = 2;
+    }
+
+    return slot;
+}
+
 int main(int argc, char **argv) {
     gfxInitDefault();
     consoleInit(GFX_BOTTOM, NULL);
@@ -34,6 +59,8 @@ int main(int argc, char **argv) {
         {0, 0, 0}
     };
 
+    int *currentSlot = handleInput(0, 0);
+
     // Main loop
     while (aptMainLoop())
     {
@@ -41,14 +68,20 @@ int main(int argc, char **argv) {
         hidScanInput(); 
         touchPosition touch;
         hidTouchRead(&touch);
+        
+        // Check for buttons
+        u32 kDown = hidKeysDown();
 
         printf("\x1b[1;1H%s", renderPlayfield(pf).c_str());
 
         printf("\x1b[5;1HX position: %i    ", touch.px);
         printf("\x1b[6;1HY position: %i    ", touch.py);
 
-        // Check for buttons
-        u32 kDown = hidKeysDown();
+        int *cs = handleInput(touch.px, touch.py);
+
+        if (kDown & KEY_TOUCH) pf[cs[0]][cs[1]] = 1;
+
+        printf("\x1b[7;1HDetected Slot: %i, %i    ", cs[0], cs[1]);
 
         if (kDown & KEY_DUP) pf[1][0] = 1;
         if (kDown & KEY_DLEFT) pf[0][1] = 1;
